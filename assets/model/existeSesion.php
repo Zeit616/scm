1581c1916943b10f9d33a9243150b32d49347cd4
Zeit <noriegallerenaivanalejandro@gmail.com>
2023-06-19 22:32:51 -0500

    entrega de visuales básicos

---
+++ assets/model/existeSesion.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+if(empty($_SESSION["Nombre"])){
+    //acceso no autorizado
+    header("Location: ../../index.html?error=4");
+}else{        
+    $Nombre = $_SESSION["Nombre"];
+    $Rol = $_SESSION["Rol"];
+    switch ($Rol) {
+        case 'Admin':
+            header("Location: ../vistas/admin/vistaGenerica.php");
+            break;
+        case 'Periodista':
+            header("Location: ../vistas/periodista/vistaGenerica.php");
+            break;
+        case 'Usuario':
+            header("Location: ../vistas/usuario/vistaGenerica.php");
+            break;
+        
+        default:
+        header("Location: ../../index.html?error=5");
+            break;
+    }
+    
+}
+
+?>
